acad058df20a735da4b6636385270dc60f11515a
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-07-18 18:31:21 -0300

    refactor: change index page

---
+++ src/styles/pages/index.styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { rem } from '../variables';
+
+export const Container = styled.main`
+  max-width: 80%;
+  margin: 0 auto;
+  position: relative;
+  padding: ${rem(88)};
+`;
