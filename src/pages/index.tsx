9d8f911dc4e4c0baf67cd26df65c44865ab475bd
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-07-19 19:16:43 -0300

    feat: add skill section to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/resources/Sections/HeroSection';
+import Skills from '@/resources/Sections/Skills';
 import { Container } from '@/styles/pages/index.styled';
 
 export default function Home() {
   return (
     <Container>
       <HeroSection />
+      <Skills />
     </Container>
   );
 }
