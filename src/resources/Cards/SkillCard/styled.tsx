f21b5854bfe48dcf59436d137826fa051ed957e7
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-07-21 13:46:12 -0300

    feat: improve skill card to dark theme and mobile

--- src/resources/Cards/SkillCard/styled.tsx
+++ src/resources/Cards/SkillCard/styled.tsx
@@ -1,11 +1,30 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Card } from '@nextui-org/react';
 
-export const CardStyled = styled(Card)`
+export const CardStyled = styled(Card)<{ selected?: boolean; dark?: boolean }>`
   max-width: 90px;
   padding: 0.8rem;
   border: 2px solid #4277ff;
   box-sizing: border-box;
+
+  ${({ selected, dark }) =>
+    selected &&
+    css`
+      &::before {
+        content: '';
+        position: absolute;
+        height: 10px;
+        width: 10px;
+        background-color: #4277ff;
+        top: 6px;
+        right: 6px;
+        border-radius: 50%;
+      }
+      box-shadow: ${dark
+        ? 'rgba(255, 255, 255, 0.43) 0px 2px 8px'
+        : 'rgba(0, 0, 0, 0.24) 0px 3px 8px'};
+    `}
 
   @media (min-width: 600px) {
     max-width: 130px;
