f2170eddb342c2c63ffdbca313dd8d4db894ebea
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-07-21 12:31:24 -0300

    feat: apply theme change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import { NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
 import '@/styles/globals.css';
 
 import { League_Spartan } from 'next/font/google';
@@ -8,16 +9,40 @@ const leagueSpartan = League_Spartan({ subsets: ['latin'] });
 
 import Header from '@/resources/Header';
 
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {},
+});
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {
+      background: '#191919',
+      backgroundAlpha: '#191919',
+    },
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Head>
-        <title>Luiz Doretto | Portfolio</title>
-      </Head>
-      <main className={leagueSpartan.className}>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Head>
+          <title>Luiz Doretto | Portfolio</title>
+        </Head>
+        <main className={leagueSpartan.className}>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
