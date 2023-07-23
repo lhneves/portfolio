2d9f47d0a567264c1a2c304c0053fa3fb2c9083b
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-07-23 16:58:08 -0300

    feat: always scroll to top

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
@@ -25,6 +27,12 @@ const darkTheme = createTheme({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <NextThemesProvider
       defaultTheme="system"
