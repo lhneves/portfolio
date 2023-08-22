3f5e7459938216aa5d296321ccbb10691da4629d
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-08-22 20:26:00 -0300

    refactor(head): add icons and remove meta tag from app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,13 +14,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {CssBaseline.flush()}
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0 maximum-scale=1"
-          ></meta>
-        </Head>
+        <Head>{CssBaseline.flush()}</Head>
         <body>
           <Main />
           <NextScript />
